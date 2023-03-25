c6647dc2c3fc1c96a56c48de4fd218201d1318ca
amit <akumar69141@gmail.com>
2023-03-26 02:50:55 +0530

    add i value

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -10,6 +10,7 @@ public class Application {
 	public static void main(String[] args) {
 
 		int j=200;
+		int i=6855;
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 		System.out.println(context.getClass().getName());
 		System.out.println("beans load:"+context.getBeanDefinitionCount());
